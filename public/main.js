60fe23ba2a33b724acb4fcccbb0343c4bcd75910
Ryan Morgan <Zerkyn.55@gmail.com>
2023-03-23 15:26:46 -0600

    initial commit

---
+++ public/main.js
@@ -0,0 +1,17 @@
+
+
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const div = document.querySelector('div')
+
+const enterInput = (event) => {
+    event.preventDefault()
+    let list = document.createElement('ul')
+    let listItem = document.createElement('li')
+    listItem.textContent = input.value
+    list.appendChild(listItem)
+    div.appendChild(list)
+    input.value = ''
+}
+
+button.addEventListener('click', enterInput)
